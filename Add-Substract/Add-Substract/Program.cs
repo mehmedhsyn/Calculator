de1b9ee90a5a58c61e0edc5743f375695ff53e3a
mehmedh <mhofficial949@gmail.com>
2021-02-01 21:36:02 +0200

    Added add/substract functions

---
+++ Add-Substract/Add-Substract/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Add_Substract
+{
+    class Program
+    {
+        static decimal Calculate(decimal number1, decimal number2, string operation)
+        {
+            if (operation == "+")
+            {
+                return Add(number1, number2);
+            }
+            else if (operation == "-")
+            {
+                return Substract(number1, number2);
+            }
+            
+            else
+            {
+                return 0;
+            }
+        }
+         static decimal Substract(decimal number1, decimal number2)
+         {
+            return number1 - number2;
+         }
+
+        static decimal Add(decimal number1, decimal number2)
+        {
+            return number1 + number2;
+        }
+
+        static void Main(string[] args)
+        {
+            decimal number1 = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            decimal number2 = int.Parse(Console.ReadLine());
+
+            decimal result = Calculate(number1, number2, operation);
+            Console.WriteLine(result);
+
+        }
+    }
+}
